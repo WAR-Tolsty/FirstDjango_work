6b27e869d52ecb7221ab928da068dfc7e88ea2cd
Mikhail Stukalov <ya@mstukalov.ru>
2025-03-25 16:27:05 +0300

    Add Description

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,3 +6,4 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveBigIntegerField()
+    description = models.TextField(max_length=200, default='Описание элемента')
